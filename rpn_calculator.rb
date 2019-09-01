58014ac87ed154b49f3d72fa4007b6ff5ef4789f
sarahsakordaniels <ssdaniels13@gmail.com>
2019-09-01 16:17:07 -0600

    Create calculator initialization page
    Upon booting up application, a list of options is presented to the user.

--- rpn_calculator.rb
+++ rpn_calculator.rb
@@ -1,2 +1,24 @@
+
+def calculate
+    puts "Enter a problem"
+    input = gets.chomp
+    input.split(//).each do |char|
+      puts char
+  end
+end
+
 puts "Welcome to the Reverse Polish Notation (RPN) Calculator!"
-puts "This is going to be legit."
+puts "Please select from the following options:"
+puts "1. Open calculator"
+puts "2. How do I use this calculator?"
+option = gets.chomp
+
+
+if option == "2"
+  puts "This calculator evaluates math problems expressed in Reverse Polish Notation (RPN)."
+  puts "RPN is a method for representing expressions where the operator symbol is placed after the arguments being operated on"
+  puts "Example:"
+  puts "'3+1' is expressed as '3 1 +'"
+elsif option == "1"
+  calculate
+end
