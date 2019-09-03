0a23cd6693502aa4a811216444acac17487bd2f0
sarahsakordaniels <ssdaniels13@gmail.com>
2019-09-03 14:07:24 -0600

    Create calculator class

--- bin/run.rb
+++ bin/run.rb
@@ -2,4 +2,5 @@ require_relative "../lib/calculator.rb"
 
 puts "\n\nWelcome to the Reverse Polish Notation (RPN) Calculator!\n\n"
 
-main_menu()
+new_session = Calculator.new
+new_session.main_menu
--- lib/calculator.rb
+++ lib/calculator.rb
@@ -1,77 +1,72 @@
-def operate(a, b, operand)
-  case operand
-  when '+'
-    b + a
-  when '-'
-    b - a
-  when '*'
-    b * a
-  when '/'
-    b / a
+class Calculator
+  def operate(a, b, operand)
+    case operand
+    when '+'
+      b + a
+    when '-'
+      b - a
+    when '*'
+      b * a
+    when '/'
+      b / a
+    end
   end
-end
 
-def calculate
-  stack = []
-  loop do
-    input = gets.chomp
-    case input
+  def calculate
+    stack = []
+    loop do
+      input = gets.chomp
+      case input
       when /\d/
         stack << input.to_i
       when /\*|\-|\+|\//
-          if stack.count >= 2
-            stack.push(operate(stack.pop, stack.pop, input))
-            puts stack.last
-          if stack.count < 2
-              puts "Insufficient operands. Please enter another operand before entering another operator."
-          end
+        if stack.count >= 2
+          stack.push(operate(stack.pop, stack.pop, input))
+          puts stack.last
+        else
+            puts "Insufficient operands. Please enter another operand before entering another operator (+, -, /, *)."
         end
       when 'q'
         main_menu
       else
-        puts "Invalid input. Please enter an opreand or operator."
-        stack.push(operate(stack.pop, stack.pop, input))
-        puts stack.last
-      when 'q'
-        main_menu
-      else
-        puts "Invalid input. Please enter a number or operator."
+        puts "Invalid input. Please enter an opreand or operator (+, -, /, *)."
+      end
     end
   end
-end
 
-def main_menu
-  puts "-----------------Main Menu--------------"
-  puts "1. Open calculator"
-  puts "2. How do I use this calculator?"
-  puts "----------------------------------------"
-  puts "\nType 'q' to exit."
+  def main_menu
+    puts "-----------------Main Menu--------------"
+    puts "1. Open calculator"
+    puts "2. How do I use this calculator?"
+    puts "----------------------------------------"
+    puts "\nType 'q' to exit."
 
-  response = gets.chomp
+    response = gets.chomp
 
-  if response == "1"
-    puts "\n\nPlease enter an operand or operator (+, -, /, *). Type 'q' to exit."
-    puts "\n\nPlease enter a number or operator symbol (+, -, /, *). Type 'q' to exit."
-    calculate
+    if response == "1"
+      puts "\n\nPlease enter an operand or operator (+, -, /, *). Type 'q' to exit."
+      calculate
 
-  elsif response == "2"
-    puts "-------------How To Use This Calculator-------------"
-    puts "This calculator evaluates math problems expressed in Reverse Polish Notation (RPN)."
-    puts "RPN is a method for representing expressions where the operator symbol is placed after the numbers being operated on.\n\n"
-    puts "Example: '3+1' is expressed as '3 1 +'\n\n"
-    puts "When using this calculator, enter a single number or operator per line."
-    puts "-----------------------------------------------------\n\n"
-    main_menu
-  elsif response == "q"
-    quit
+    elsif response == "2"
+      puts "-------------How To Use This Calculator-------------"
+      puts "This calculator evaluates math problems expressed in Reverse Polish Notation (RPN)."
+      puts "RPN is a method for representing expressions where the operator symbol is placed after the numbers being operated on.\n\n"
+      puts "Example: '3+1' is expressed as '3 1 +'\n\n"
+      puts "When using this calculator, enter a single number or operator per line."
+      puts "Valid operators for this calculator include +, -, /, *"
+      puts "-----------------------------------------------------\n\n"
+      main_menu
+    elsif response == "q"
+      quit
 
-  else
-    puts "\n\nInvalid option. Please enter an option from the menu below.\n\n"
-    main_menu
+    else
+      puts "\n\nInvalid option. Please enter an option from the menu below.\n\n"
+      main_menu
+    end
   end
-end
 
-def quit
-  puts "\n\nBye!"
-  exit
+  def quit
+    puts "\n\nBye!"
+    exit
+  end
 end
